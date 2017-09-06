546f0c63baee102581d4fc2d82888f5ecde12a09
elpinal <elpinal@users.noreply.github.com>
2017-09-06 22:47:27 +0900

    Implement help

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,48 @@
 module Lib
-    ( someFunc
+    ( run
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Environment
+import System.Exit
+import System.IO
+
+repoURI :: String
+repoURI = "https://github.com/ruby/ruby"
+
+run :: IO ()
+run = do
+  args <- getArgs
+  parseArgs args
+
+failWith :: String -> IO ()
+failWith msg = hPutStrLn stderr msg >> exitFailure
+
+parseArgs :: [String] -> IO ()
+parseArgs [] = putStrLn usage >> exitFailure
+parseArgs (name:args) = cmd name args
+  where
+    cmd "help" = help
+    cmd x = \_ -> failWith $ "monumental-ruby: no such command " ++ show x
+
+usage :: String
+usage =
+  unlines [ "Monumental-ruby is a tool for managing various versions of Ruby."
+          , ""
+          , "Usage:"
+          , ""
+          , "        help         show help"
+          , ""
+          ]
+
+help :: [String] -> IO ()
+help [] = putStrLn usage
+help [topic] = helpOf topic
+  where
+    helpOf "help" = putStrLn "usage: monumental-ruby help [topic]"
+    helpOf topic = failWith $ "unknown help topic " ++ show topic ++ ". Run 'monumental-ruby help'."
+help _ =
+  failWith $
+    unlines [ "usage: monumental-ruby help command"
+            , ""
+            , "Too many arguments given."
+            ]

3035a163bdc07a3a11954e27d28cbf8c6308b1d5
elpinal <elpinal@users.noreply.github.com>
2017-09-10 21:01:42 +0900

    Add test for parseFlag

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -3,6 +3,10 @@ module LibSpec (spec) where
 import Test.Hspec
 import Lib
 
+import Control.Monad.Except
+import Control.Monad.State.Lazy
+import Data.Either
+
 spec :: Spec
 spec = do
   describe "getDest" $ do
@@ -13,3 +17,16 @@ spec = do
     it "gets root path from home path" $ do
       let home = "home"
       rootPath home `shouldBe` "home/.monumental-ruby"
+
+  describe "parseFlag" $ do
+    it "parses flags in args" $ do
+      let xs = ["-h", "-root", "aaa", "arg1", "arg2"]
+      let (flags, args) = runState (runExceptT parseFlag) xs
+      flags `shouldBe` Right [Help, Root "aaa"]
+      args `shouldBe` ["arg1", "arg2"]
+
+    it "returns an error when given flag does not exist" $ do
+      let xs = ["-no-such-flag", "arg"]
+      let (flags, args) = runState (runExceptT parseFlag) xs
+      isLeft flags `shouldBe` True
+      args `shouldBe` ["-no-such-flag", "arg"]

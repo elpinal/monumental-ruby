57aa52535f93759c7015b40c06b826b6d81de250
elpinal <elpinal@users.noreply.github.com>
2017-09-22 21:11:53 +0900

    Rename TestSym -> TestIO

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -11,24 +11,24 @@ import Data.Either
 import Data.Maybe
 import qualified Data.Map.Lazy as Map
 
-newtype TestSym a = TestSym (Reader FileMap a)
+newtype TestIO a = TestIO (Reader FileMap a)
 
-runTestSym :: TestSym a -> Reader FileMap a
-runTestSym (TestSym x) = x
+runTestIO :: TestIO a -> Reader FileMap a
+runTestIO (TestIO x) = x
 
-instance Functor TestSym where
-  fmap f (TestSym x) = TestSym $ fmap f x
+instance Functor TestIO where
+  fmap f (TestIO x) = TestIO $ fmap f x
 
-instance Applicative TestSym where
-  pure = TestSym . reader . const
-  (TestSym f) <*> (TestSym x) = TestSym $ f <*> x
+instance Applicative TestIO where
+  pure = TestIO . reader . const
+  (TestIO f) <*> (TestIO x) = TestIO $ f <*> x
 
-instance Monad TestSym where
-  (TestSym x) >>= f = TestSym $ x >>= runTestSym . f
+instance Monad TestIO where
+  (TestIO x) >>= f = TestIO $ x >>= runTestIO . f
 
-instance MonadSym TestSym where
-  readSym p = MaybeT . TestSym $ Map.lookup p . symMap <$> ask
-  listDir p = MaybeT . TestSym $ Map.lookup p . dirMap <$> ask
+instance MonadSym TestIO where
+  readSym p = MaybeT . TestIO $ Map.lookup p . symMap <$> ask
+  listDir p = MaybeT . TestIO $ Map.lookup p . dirMap <$> ask
 
 data FileMap = FileMap
   { symMap :: Map.Map FilePath FilePath
@@ -68,4 +68,4 @@ spec = do
       let m = FileMap { symMap = Map.singleton "root/bin" "foo/bar/v2_3_4/baz"
                       , dirMap = Map.empty
                       }
-      runReader (runTestSym . runMaybeT $ getActive "root") m `shouldBe` Just "v2_3_4"
+      runReader (runTestIO . runMaybeT $ getActive "root") m `shouldBe` Just "v2_3_4"
